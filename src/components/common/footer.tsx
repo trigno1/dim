52633464eff270a2363a34d3bcf6aee191ca9144
Tanish Sunita Pareek <tanishpareek2005@gmail.com>
2025-09-21 09:04:46 +0000

    yeah like thus but use previous format

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -21,7 +21,7 @@ export function Footer() {
       </div>
 
       <div className="container mx-auto px-4 md:px-6">
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8 text-sm items-center">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-8 text-sm items-start text-center md:text-left">
 
           <div>
              <h3 className="font-semibold text-lg mb-4 text-secondary-foreground">Useful Links</h3>
